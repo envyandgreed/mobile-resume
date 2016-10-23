7b711affea41cfd600e52f5c0d53bdb95bd3ae86
envyandgreed <825011977@qq.com>
2016-10-23 19:09:32 +0800

    add

---
+++ h5.js
@@ -0,0 +1,49 @@
+var H5 = function ( ) {
+    this.id = ('h5_'+Math.random()).replace('.','_')
+    this.el = $('<div class="h5" id="'+this.id+'">').hide()
+    this.page = []
+    $('body').append( this.el )
+
+    // 新增一个页
+    // name 组建的名字，会加入到Classname中
+    // text    文本内容
+    this.addPage = function (name,text){
+       var page = $('<div class="h5_page section">')
+
+        if (name != undefined){
+            page.addClass('h5_page_' + name)
+        }
+        if (text != undefined){
+            page.text(text)
+        }
+        this.el.append(page)
+        this.page.push( page )
+        return this
+    }
+
+    this.addComponent = function (name,cfg){
+        var cfg = cfg || {};
+        cfg = $.extend({
+            type :' base'
+        },cfg);
+
+        var component;
+        var page = this.page.slice(-1)[0]
+        console.log(page)
+
+        switch (cfg.type){
+            case 'base':
+                component = new H5ComponentBase(name,cfg);
+            break;
+            default:
+        }
+        page.append(component);
+        return this
+    }
+
+    this.loader = function (){
+        this.el.fullpage();
+        this.el.show()
+    }
+    return this
+}
